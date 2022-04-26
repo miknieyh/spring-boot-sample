9a3827e9a8fa22702d706b0aef23ab9a4e621900
miknieyh <tong_hye@naver.com>
2022-04-26 09:31:42 +0900

    test코드 작성중

--- src/test/java/com/sample/springbootsample/SpringBootSampleApplicationTests.java
+++ src/test/java/com/sample/springbootsample/SpringBootSampleApplicationTests.java
@@ -1,13 +1,41 @@
 package com.sample.springbootsample;
 
+import com.sample.springbootsample.domain.SanhaItRepository;
+import com.sample.springbootsample.domain.YanoljaRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootSampleApplicationTests {
 
+     @LocalServerPort
+     private int port;
+
+     @Autowired
+     private SanhaItRepository sanhaItRepository;
+
+     @Autowired
+     private YanoljaRepository yanoljaRepository;
+
+     @AfterEach
+     public void tearDown() throws Exception {
+          sanhaItRepository.deleteAll();
+          yanoljaRepository.deleteAll();
+     }
+
      @Test
-     void contextLoads() {
+     void Yanolja_save() {
+          //given
+
+          //when
+
+          //then
      }
 
 }
